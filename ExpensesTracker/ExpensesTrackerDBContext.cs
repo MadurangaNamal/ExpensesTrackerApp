42eac9f6e8bea5139d7d14f95741a4bc1b325fcb
maduranga <maduranga.wimalarathne95@gmail.com>
2025-06-30 20:49:45 +0400

    Initial ORM & model setup

---
+++ ExpensesTracker/ExpensesTrackerDBContext.cs
@@ -0,0 +1,21 @@
+﻿using ExpensesTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTracker;
+
+public class ExpensesTrackerDBContext : DbContext
+{
+    public ExpensesTrackerDBContext(DbContextOptions<ExpensesTrackerDBContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Expense> Expenses { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Expense>()
+            .ToTable("Expenses");
+        base.OnModelCreating(modelBuilder);
+    }
+}
