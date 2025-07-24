5f5e8c509a61a7978cc5c8a28cb8203cdfc34213
maduranga <maduranga.wimalarathne95@gmail.com>
2025-07-24 16:07:25 +0400

    Configure Google Authentication

--- ExpensesTracker/Program.cs
+++ ExpensesTracker/Program.cs
@@ -1,9 +1,42 @@
 using ExpensesTracker.Data;
 using ExpensesTracker.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+})
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"] ?? throw new InvalidOperationException("Google ClientId is not configured.");
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"] ?? throw new InvalidOperationException("Google ClientSecret is not configured.");
+
+    //Request additional scopes (permissions) for email and profile information from google
+    options.Scope.Add("email");
+    options.Scope.Add("profile");
+
+    // On successful authentication
+    options.Events.OnCreatingTicket = context =>
+    {
+        var userEmail = context.Principal!.FindFirst(ClaimTypes.Email)?.Value;
+        var userName = context.Principal.FindFirst(ClaimTypes.Name)?.Value;
+
+        return Task.CompletedTask;
+    };
+});
 
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(options =>
@@ -25,14 +58,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
 app.MapStaticAssets();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
